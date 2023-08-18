142506ea430e1e45c16ba1ba1277178c4e6ac85f
flowfordan <flowfordan@gmail.com>
2023-08-18 21:24:35 +0700

    fix: css mixins setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="light">
       <body className={inter.className}>{children}</body>
     </html>
   );
